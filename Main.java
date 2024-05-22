30a573b98065811c8bcf21e49e0d639137f6dbb4
Eduarda Portel <eduarda.portel4@gmail.com>
2024-05-22 16:34:16 -0300

    stack created and 3 pushs done

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+    // creating a Stack that can store String
+    Stack<String> stack = new Stack<String>(); // add a construtor()
+
+    // empty method
+    // System.out.println(stack.empty());
+
+    // push : used to add
+    stack.push("Minecraft");
+    stack.push("Valorant");
+    stack.push("League of Legends");
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# stack-opperations" 
+stack - LIFO data structure
